d27c321175cbe8cea0276048c72d1cfc2b333f66
Salinas <80262440+eoSalinas@users.noreply.github.com>
2025-12-28 18:45:17 -0300

    chore(webapps): remove figma

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -2,5 +2,4 @@ ginga-webapp-install "Google Photos" https://photos.google.com/ "Google Photos.p
 ginga-webapp-install "YouTube" https://youtube.com/ YouTube.png
 ginga-webapp-install "GitHub" https://github.com/ GitHub.png
 ginga-webapp-install "X" https://x.com/ X.png
-ginga-webapp-install "Figma" https://figma.com/ Figma.png
 ginga-webapp-install "Zoom" https://app.zoom.us/wc/home Zoom.png "ginga-webapp-handler-zoom %u" "x-scheme-handler/zoommtg;x-scheme-handler/zoomus"

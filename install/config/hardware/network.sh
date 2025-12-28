4c636f6726636070114579749e0fc3aa6099f9d3
Salinas <80262440+eoSalinas@users.noreply.github.com>
2025-12-28 12:46:27 -0300

    chore(network): remove not working as expected

--- install/config/hardware/network.sh
+++ install/config/hardware/network.sh
@@ -1,7 +1,3 @@
 # Ensure iwd service will be started
 sudo systemctl enable iwd.service
 
-# TODO: evaluate this cause its not working as expected
-# Prevent systemd-networkd-wait-online timeout on boot
-# sudo systemctl disable systemd-networkd-wait-online.service
-# sudo systemctl mask systemd-networkd-wait-online.service
